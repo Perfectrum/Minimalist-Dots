53df8b5efe359637278f469e68d4a2ada5147535
Eloysheyin <70163259+Eloysheyin@users.noreply.github.com>
2020-09-03 08:55:23 +0530

    Update launch.sh
    added some more bars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,8 +8,12 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar left >>/tmp/polybar1.log 2>&1 &
+polybar player >>/tmp/polybar1.log 2>&1 &
 polybar center >>/tmp/polybar2.log 2>&1 &
 polybar right >>/tmp/polybar3.log 2>&1 &
+polybar songs >>/tmp/polybar1.log 2>&1 &
+polybar launcher >>/tmp/polybar1.log 2>&1 &
+polybar power >>/tmp/polybar1.log 2>&1 &
 
 
 echo "Bars launched..."
