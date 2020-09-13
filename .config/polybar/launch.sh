0611c6074f575f0b6e96188b3dd8044bad4372dd
Eloysheyin <70163259+Eloysheyin@users.noreply.github.com>
2020-09-13 18:18:07 +0530

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,19 +1,18 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+## Add this to your wm startup file.
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar left >>/tmp/polybar1.log 2>&1 &
-polybar player >>/tmp/polybar1.log 2>&1 &
-polybar center >>/tmp/polybar2.log 2>&1 &
-polybar right >>/tmp/polybar3.log 2>&1 &
-polybar songs >>/tmp/polybar1.log 2>&1 &
-polybar launcher >>/tmp/polybar1.log 2>&1 &
-polybar power >>/tmp/polybar1.log 2>&1 &
+## Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-
-echo "Bars launched..."
+## Launch
+MONITOR=eDP0 polybar -c ~/.config/polybar/config launcher &
+MONITOR=eDP0 polybar -c ~/.config/polybar/config songs &
+MONITOR=eDP0 polybar -c ~/.config/polybar/config player &
+MONITOR=eDP0 polybar -c ~/.config/polybar/config left &
+MONITOR=eDP0 polybar -c ~/.config/polybar/config center &
+MONITOR=eDP0 polybar -c ~/.config/polybar/config right &
+MONITOR=eDP0 polybar -c ~/.config/polybar/config power &
